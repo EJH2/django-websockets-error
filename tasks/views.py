60492392d4900ecdcf89dde4ccb0352737fddf5d
Ethan <ethan.harriz@gmail.com>
2020-08-30 09:55:36 -0400

    Modify project files to strip away clutter for example

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
+from celery import chain
 
-from .tasks import http_task, http_error_task, ws_task, ws_error_task
+from .tasks import ws_task, ws_error_task
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-def http_view(request):
-    result = http_task.delay(number=100)
-    return render(request, 'http.html', context={'task_id': result.task_id})
-
-
-def http_error_view(request):
-    result = http_error_task.delay(number=100)
-    return render(request, 'http.html', context={'task_id': result.task_id})
-
-
 def ws_view(request):
-    result = ws_task.delay(number=100)
-    return render(request, 'ws.html', context={'task_id': result.task_id})
+    result = chain(ws_task.s(number=10) | ws_task.s(number=10) | ws_task.s(number=10) | ws_task.s(number=10) | ws_task.s(number=10))()
+    id_chain = []
+    while result.parent:
+        id_chain.insert(0, result.id)
+        result = result.parent
+    id_chain.insert(0, result.id)
+    return render(request, 'ws.html', context={'task_ids': id_chain})
 
 
 def ws_error_view(request):
-    result = ws_error_task.delay(number=100)
-    return render(request, 'ws.html', context={'task_id': result.task_id})
+    result = chain(ws_error_task.s(number=10) | ws_error_task.s(number=10) | ws_error_task.s(number=10) | ws_error_task.s(number=10) | ws_error_task.s(number=10))()
+    id_chain = []
+    while result.parent:
+        id_chain.insert(0, result.id)
+        result = result.parent
+    id_chain.insert(0, result.id)
+    return render(request, 'ws.html', context={'task_ids': id_chain})
